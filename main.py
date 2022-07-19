e8b2c2b743619c326d03c43bbb64af46aee75b20
MaximeVives <44499746+MaximeVives@users.noreply.github.com>
2022-07-19 12:21:05 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
 import itertools
 
-inp = [3,2,1]
+input = [3, 2, 1]
 
-def findNextBigPerm(input):
-    # find all permutation
-    all_perm = list(itertools.permutations(input))
-    # sort from lower to higher
-    all_perm.sort()
-    # transform each one to a number
-    all_int = []
-    for perm in all_perm:
-        i = int(''.join(str(x) for x in perm))
-        all_int.append(i)
-    
-    # find the initial number 
-    init_number = all_int.index(int("".join(str(x) for x in input)))
+all_perm = [list(t) for t in itertools.permutations(input)]
+all_perm.sort()
 
-
-    # if the initial number is the last in the list, get the first number
-    # else get the next one in the list 
-    next_one = all_int[(init_number + 1) % len(all_int)]
-    # transform the number in a list
-    return [int(x) for x in str(next_one)]
-
-print(findNextBigPerm(inp))
+print(all_perm)
